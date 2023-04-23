a43823d8939864068190cbe04ec40a49ed173496
Mark Anthony Llego <lr.mallego@mmdc.mcl.edu.ph>
2023-04-23 18:39:17 +0800

    fix

--- script.sh
+++ script.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+trap on_exit INT
+
+on_exit() {
+  echo "Terminating all download processes..."
+  for pid in "${pids[@]}"; do
+    kill "$pid" 2>/dev/null
+  done
+}
 
 # Check if the input file is provided
 if [ "$#" -ne 1 ]; then
@@ -14,5 +22,16 @@ if [ ! -f "$input_file" ]; then
   exit 1
 fi
 
+declare -a pids
+
 # Filter out empty lines and lines starting with '#' (considered as comments)
-grep -v '^$\|^\s*\#' "$input_file" | xargs -I {} -P 4 -n 1 bash download_file.sh {}
+while IFS= read -r url; do
+  if [ -n "$url" ] && [[ ! "$url" =~ ^[[:space:]]*\# ]]; then
+    bash download_file.sh "$url" &
+    pids+=("$!")
+  fi
+done < "$input_file"
+
+wait
+
+echo "All downloads are completed or terminated."
